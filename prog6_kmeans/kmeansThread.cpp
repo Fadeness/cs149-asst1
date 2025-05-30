831912a834e3a171c86c9bc2b4e53fb9e4aa23c5
Fadeness <Fadeness@qq.com>
2025-05-30 15:35:44 +0800

    prog6: solutions

--- prog6_kmeans/kmeansThread.cpp
+++ prog6_kmeans/kmeansThread.cpp
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <thread>
+#include <immintrin.h>
 
 #include "CycleTimer.h"
 
@@ -18,8 +19,12 @@ typedef struct {
   int *clusterAssignments;
   double *currCost;
   int M, N, K;
-} WorkerArgs;
 
+  // New args
+  int threadId;
+  double* minDist;
+  int mStart, mEnd;
+} WorkerArgs;
 
 /**
  * Checks if the algorithm has converged.
@@ -65,6 +70,7 @@ double dist(double *x, double *y, int nDim) {
  * Assigns each data point to its "closest" cluster centroid.
  */
 void computeAssignments(WorkerArgs *const args) {
+  /*
   double *minDist = new double[args->M];
   
   // Initialize arrays
@@ -72,20 +78,20 @@ void computeAssignments(WorkerArgs *const args) {
     minDist[m] = 1e30;
     args->clusterAssignments[m] = -1;
   }
-
+  */
   // Assign datapoints to closest centroids
   for (int k = args->start; k < args->end; k++) {
-    for (int m = 0; m < args->M; m++) {
+    for (int m = args->mStart; m < args->mEnd; m++) {
       double d = dist(&args->data[m * args->N],
                       &args->clusterCentroids[k * args->N], args->N);
-      if (d < minDist[m]) {
-        minDist[m] = d;
+      if (d < args->minDist[m]) {
+        args->minDist[m] = d;
         args->clusterAssignments[m] = k;
       }
     }
   }
 
-  free(minDist);
+  // free(minDist);
 }
 
 /**
@@ -151,6 +157,67 @@ void computeCost(WorkerArgs *const args) {
   free(accum);
 }
 
+void workerThreadStart(WorkerArgs *const args)
+{
+  double startTime {CycleTimer::currentSeconds()};
+  computeAssignments(args);
+  double endTime {CycleTimer::currentSeconds()};
+  printf("Thread %d spends %.2lf ms to finish the task\n", args->threadId, endTime - startTime);
+}
+
+void computeAssignmentsThread(WorkerArgs *const args) {
+  double *minDist = new double[args->M];
+  
+  // Initialize arrays
+  for (int m =0; m < args->M; m++) {
+    minDist[m] = 1e30;
+    args->clusterAssignments[m] = -1;
+  }
+
+  int NUM_THREADS = 12;
+
+  std::thread workers[NUM_THREADS];
+  WorkerArgs thread_args[NUM_THREADS];
+
+  printf("%d %d\n", args->K, args->K / NUM_THREADS);
+
+  for (int i = 0; i < NUM_THREADS; ++i)
+  {
+    thread_args[i].data = args->data;
+    thread_args[i].clusterAssignments = args->clusterAssignments;
+    thread_args[i].clusterCentroids = args->clusterCentroids;
+    thread_args[i].K = args->K;
+    thread_args[i].M = args->M;
+    thread_args[i].N = args->N;
+    thread_args[i].start = args->start;
+    thread_args[i].end = args->end;
+    thread_args[i].threadId = i;
+    thread_args[i].minDist = minDist;
+
+    thread_args[i].mStart = i * (args->M / NUM_THREADS);
+    if (i == NUM_THREADS - 1 && args->M % NUM_THREADS != 0)
+    {
+      thread_args[i].mEnd = args->M;
+    }
+    else
+    {
+      thread_args[i].mEnd = (i + 1) * (args->M / NUM_THREADS);
+    }
+  }
+
+  for (int i = 1; i < NUM_THREADS; ++i)
+  {
+    workers[i] = std::thread(workerThreadStart, &thread_args[i]);
+  }
+
+  workerThreadStart(&thread_args[0]);
+
+  for (int i = 1; i < NUM_THREADS; ++i)
+  {
+    workers[i].join();
+  }
+}
+
 /**
  * Computes the K-Means algorithm, using std::thread to parallelize the work.
  *
@@ -207,9 +274,20 @@ void kMeansThread(double *data, double *clusterCentroids, int *clusterAssignment
     args.start = 0;
     args.end = K;
 
-    computeAssignments(&args);
+    double startTime {CycleTimer::currentSeconds()};
+
+    // computeAssignments(&args);
+    computeAssignmentsThread(&args);
+    double endTime {CycleTimer::currentSeconds()};
+    printf("Assignments Computation took %lf seconds to finish.\n", endTime - startTime);
+    startTime = CycleTimer::currentSeconds();
     computeCentroids(&args);
+    endTime = CycleTimer::currentSeconds();
+    printf("Centroids Computation took %lf seconds to finish.\n", endTime - startTime);
+    startTime = CycleTimer::currentSeconds();
     computeCost(&args);
+    endTime = CycleTimer::currentSeconds();
+    printf("Cost Computation took %lf seconds to finish.\n", endTime - startTime);
 
     iter++;
   }
